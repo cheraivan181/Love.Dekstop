274013ff8d286a8444215591537136d7a6b61d07
ilyasamuray <voloshin.ilya.andreevich@gmail.com>
2021-05-10 19:33:20 +0300

    add message view

---
+++ Love.Dekstop/ChatTemplateSelector.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Love.Dekstop
+{
+    public class ChatTemplateSelector : DataTemplateSelector
+    {
+        DataTemplate incomingDataTemplate;
+        DataTemplate outgoingDataTemplate;
+
+        public ChatTemplateSelector()
+        {
+            this.incomingDataTemplate = new DataTemplate(typeof(IncomingTemplate));
+            this.outgoingDataTemplate = new DataTemplate(typeof(OutgoingTemplate));
+        }
+
+        public override DataTemplate SelectTemplate(object item, DependencyObject container)
+        {
+            FrameworkElement element = container as FrameworkElement;
+          
+            var messageVm = item as TestMessage;
+            if (messageVm == null)
+                return null;
+
+            if (messageVm.Sender == ContactsForm.Iam)
+                return outgoingDataTemplate;
+            else
+                return incomingDataTemplate;
+        }
+    }
+}
--- Love.Dekstop/ContactsForm.xaml.cs
+++ Love.Dekstop/ContactsForm.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -17,9 +18,59 @@ namespace Love.Dekstop
     /// </summary>
     public partial class ContactsForm : Window
     {
+        private ObservableCollection<TestContacts> contacts;
+        private ObservableCollection<TestMessage> selectedMessages;
+        private TestContacts selectedContact;
+
+        public static string Iam = "Ilya";
         public ContactsForm()
         {
             InitializeComponent();
+            contacts = new ObservableCollection<TestContacts>
+            {
+                new TestContacts(){Name="Pendos",LastMessage = "Sosi",
+                    Messages = new ObservableCollection<TestMessage>
+                    {
+                        new TestMessage(){Sender = "Pendos",Text = "Sosi"},
+                        new TestMessage(){Sender = Iam, Text = "Sam Sosi"},
+                        new TestMessage(){Sender = "Pendos",Text = "Okey("}
+                    }},
+                new TestContacts()
+                {
+                    Name="Chera",LastMessage = "Ilya sdelai chto to",
+                    Messages = new ObservableCollection<TestMessage>
+                    {
+                        new TestMessage(){Sender = "Chera",Text = "Sosi xyi"},
+                        new TestMessage(){Sender = Iam, Text = "Sam Sosi"},
+                        new TestMessage(){Sender = "Chera",Text = "Okey("}
+                    }
+                },
+                new TestContacts(){Name="Peder",LastMessage = "Ya daun"},
+            };
+            ContactsList.ItemsSource = contacts;
+            
         }
+
+        private void ContactsList_OnSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            selectedContact = (TestContacts) ContactsList.SelectedItem;
+            selectedMessages = selectedContact.Messages;
+            ItemsControl.ItemsSource = selectedMessages;
+        }
+    }
+
+    public class TestContacts
+    {
+        public string Name { get; set; }
+        public string LastMessage { get; set; }
+
+        public ObservableCollection<TestMessage> Messages { get; set; }
+    }
+
+    public class TestMessage
+    {
+        public string Text { get; set; }
+
+        public string Sender { get; set; }
     }
 }
